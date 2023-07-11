c7c88ad8059599294f7e00647e4fe57ebb775676
honeysang <vytkdgjs12@naver.com>
2023-07-11 22:50:57 +0900

    백준 10869 사칙연산

--- src/B10869.java
+++ src/B10869.java
@@ -4,8 +4,11 @@ public class B10869 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        String input = scanner.nextLine();
+        String[] s = input.split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a * b);
